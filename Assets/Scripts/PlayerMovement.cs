7dd5d52812352938a77b264db0c913279b85243b
Edgar Pinto <edgarpinto010203@gmail.com>
2025-05-07 12:10:52 -0600

    Agregando movimiento del jugador y script de camara

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); // Movimiento izquierda/derecha
+        movement.y = Input.GetAxisRaw("Vertical");   // Movimiento arriba/abajo
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
